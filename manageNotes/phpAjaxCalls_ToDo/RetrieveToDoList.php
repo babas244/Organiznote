f388b4e9f76232b5515167d8777f497e21f291df
Pierre <fdldliu@kdsfhjk>
2017-02-19 13:07:44 +0700

    affichage de todolist par appel ajax

---
+++ manageNotes/phpAjaxCalls_ToDo/RetrieveToDoList.php
@@ -0,0 +1,37 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+session_start();
+
+if (isset($_SESSION['id']) && isset($_GET["idTopic"])) {
+
+	if (preg_match("#^[0-9]+$#", $_GET["idTopic"])) {		
+		
+		$idTopic = htmlspecialchars($_GET["idTopic"]);
+
+		include '../../log_in_bdd.php';		
+	
+		$reqDisplayToDoList = $bdd -> prepare('SELECT content, dateCreation, dateExpired FROM todolists WHERE idUser=:idUser AND idTopic=:idTopic ORDER BY dateCreation DESC');
+			$reqDisplayToDoList -> execute(array(
+			'idUser' => $_SESSION['id'],
+			'idTopic' => $_GET["idTopic"])) or die(print_r($reqDisplayToDoList->errorInfo()));
+			//echo ('<br>'.$reqDisplayToDoList->rowCount().' rangs affectés');
+			
+			$toDoFetched = "[";
+			
+			while ($data = $reqDisplayToDoList->fetch()) {
+			$toDoFetched .= '"'.$data['content'].'",';
+			}
+		$reqDisplayToDoList -> closeCursor();	
+			
+		echo $toDoFetched == "[" ? "" : substr($toDoFetched, 0, -1)."]"; //il faut enlever le dernier ","
+		
+		/*echo ('<br>'.$reqUpdateSiblingsAndChildren->rowCount()." lignes affectées dans reqUpdateSiblingsAndChildren<br>");
+		*/
+	}
+}
+
+else {
+	echo 'Une des variables n\'est pas définie ou la session n\'est pas ouverte !!!';	// ajouter du html pour que ca s'affiche comme une box !!
+}
+?>
--- manageNotes/toDoList.js
+++ manageNotes/toDoList.js
@@ -1,7 +1,45 @@
+var toDoFocused = null;
+
 document.getElementById("noscroll").addEventListener('touchmove', function(event) {
 	event.preventDefault();
 }, false);
 
+
+ajaxCall('phpAjaxCalls_ToDo/retrieveToDoList.php?idTopic=' + idTopic, displayToDoList)
+
+function ajaxCall(sPathPhp, fCallBack) {
+	var xhr = new XMLHttpRequest(); 
+	xhr.open ('GET', sPathPhp);
+	xhr.send(null);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			fCallBack(xhr.responseText);
+		} 
+		else if (xhr.readyState == 4 && xhr.status != 200) {
+				alert('Une erreur est survenue !\n\nCode:' + xhr.status + '\nTexte: ' + xhr.statusText);
+		}
+	}
+}
+				
+function displayToDoList(sToDoListRetrieved) {
+	var aToDoListJSONParsed = sToDoListRetrieved == "" ? "" : JSON.parse(sToDoListRetrieved); 
+	// if aToDoListJSONParsed =="" afficher "pas encore de notes"
+	var nbOfItemsOfToDoListJSONParsed = aToDoListJSONParsed.length; 
+	for (i = 0 ; i < nbOfItemsOfToDoListJSONParsed; i ++) {
+		var sContent = aToDoListJSONParsed[i].replace(/&lt;br&gt;/gi, "\n");
+		var oDOMToDo = document.createElement("div");
+		oDOMToDo.id = 'toDo'+(i+1);
+		oDOMToDo.addEventListener('contextmenu', function(e) {
+			e.preventDefault();
+			toDoFocused = e.target.id;
+			displayContextMenu(toDoFocused);
+		}, false);
+		oDOMToDo.innerHTML = sContent; 
+		oDOMToDo.className = "toDo";
+		document.getElementById("frameOfToDo").appendChild(oDOMToDo);
+	}
+}
+				
 
 // insérer un nouveau toDo avant le premier déjà affiché puis le placer dans la bdd
 document.getElementById("addToDo").addEventListener('click', function () {
