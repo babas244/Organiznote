07367016bd29042ce627067a76f3aa5968646e49
Pierre <fdldliu@kdsfhjk>
2017-03-19 18:44:31 +0700

    on peut afficher puis faire diapaître les toDo selon leur class

--- manageNotes/toDoList.js
+++ manageNotes/toDoList.js
@@ -4,7 +4,7 @@ var IdOfFirstToDoInitial = IdOfFirstToDo;
 
 addEventsDragAndDropToLastAndInvisible(document.getElementById("lastAndInvisible"));
 
-ajaxCall('phpAjaxCalls_ToDo/retrieveToDoList.php?idTopic=' + idTopic + "&labelTitleRank=1&labelRank=1", insertToDoListBefore, 'lastAndInvisible')
+ajaxCall('phpAjaxCalls_ToDo/retrieveToDoList.php?idTopic=' + idTopic + "&labelTitleRank=1&labelRank=1", insertToDoListBefore, 'lastAndInvisible', "1", "1")
 
 ajaxCall('phpAjaxCalls_ToDo/retrieveLabels.php?idTopic=' + idTopic, displayLabelsCheckboxes); 
 
@@ -23,13 +23,13 @@ document.getElementById("addToDoForm").addEventListener('submit', function(e) {
 	e.preventDefault();
 }, false);
 
-function ajaxCall(sPathPhp, fCallBack, parameter1) {
+function ajaxCall(sPathPhp, fCallBack, parameter1, parameter2, parameter3) {
 	var xhr = new XMLHttpRequest(); 
 	xhr.open ('GET', sPathPhp);
 	xhr.send(null);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && xhr.status == 200) {
-			fCallBack(xhr.responseText =="" ? undefined : xhr.responseText, parameter1);
+			fCallBack(xhr.responseText =="" ? undefined : xhr.responseText, parameter1, parameter2, parameter3);
 		} 
 		else if (xhr.readyState == 4 && xhr.status != 200) {
 				alert('Une erreur est survenue !\n\nCode:' + xhr.status + '\nTexte: ' + xhr.statusText);
@@ -53,37 +53,39 @@ function ajaxCallNoResponse(sPathPhp, fCallBack, parameter1, parameter2) {
 
 function displayLabelsCheckboxes(sLabelsJSON) {
 	var oLabels = JSON.parse(sLabelsJSON);
-	var rankOfTitleLabel = 1;
-	var rankOfLabel;
+	var labelTitleRank = 1;
+	var labelRank;
 	for (titleLabel in oLabels) {
-		for (var rankOfLabel=1 ; rankOfLabel < oLabels[titleLabel].length + 1 ; rankOfLabel++) {
+		for (var labelRank=1 ; labelRank < oLabels[titleLabel].length + 1 ; labelRank++) {
 			var oDOMLabelCheckbox = document.createElement("input");
 			oDOMLabelCheckbox.type = "checkbox";
-			oDOMLabelCheckbox.id = "checkboxLabel"+rankOfTitleLabel+"a"+rankOfLabel;
-			oDOMLabelCheckbox.rankOfTitleLabel = rankOfTitleLabel;
-			oDOMLabelCheckbox.rankOfLabel = rankOfLabel;
+			oDOMLabelCheckbox.id = "checkboxLabel"+labelTitleRank+"a"+labelRank;
+			oDOMLabelCheckbox.labelTitleRank = labelTitleRank;
+			oDOMLabelCheckbox.labelRank = labelRank;
 			oDOMLabelCheckbox.addEventListener('input', function (e){
-				displayToDoList(e.target.rankOfTitleLabel, e.target.rankOfLabel, e.target.checked);
+				displayToDoList(e.target.labelTitleRank, e.target.labelRank, e.target.checked);
 			}, false);
 			document.getElementById("containerOfToDo").appendChild(oDOMLabelCheckbox);
 			var oDOMDivLabel = document.createElement("span");
-			oDOMDivLabel.innerHTML = oLabels[titleLabel][rankOfLabel-1];
+			oDOMDivLabel.innerHTML = oLabels[titleLabel][labelRank-1];
 			document.getElementById("containerOfToDo").appendChild(oDOMDivLabel);				
 		}	
 		oDOMElementBr = document.createElement("Br");
 		document.getElementById("containerOfToDo").appendChild(oDOMElementBr);
 
-		rankOfTitleLabel +=1;
+		labelTitleRank +=1;
 	}
+	document.getElementById("checkboxLabel1a1").checked = true;
 }
 			
-function displayToDoList (rankOfTitleLabel, rankOfLabel, isChecked) {
-	var aDOMHasClassOfToDo = document.querySelectorAll('div[class="toDo'+rankOfTitleLabel+'a'+rankOfLabel+'"]');
+function displayToDoList (labelTitleRank, labelRank, isChecked) {
+	var aDOMHasClassOfToDo = document.querySelectorAll('.toDo'+labelTitleRank+'a'+labelRank);  //('div[classname="toDo'+labelTitleRank+'a'+labelRank+'"]'); marche pas ??
 	var numberOfToDo = aDOMHasClassOfToDo.length;
+	//alert (numberOfToDo);
 	if (isChecked) {
 		// tester d'abord s'il y a déjà un toDo de cette class déjà instancié. vaudrait-il mieux avoir une variable globale qui dit si déjà chargé ou pas ? 
 		if (numberOfToDo === 0) {
-			ajaxCall('phpAjaxCalls_ToDo/retrieveToDoList.php?idTopic=' + idTopic, insertToDoListBefore, 'lastAndInvisible')		
+			ajaxCall('phpAjaxCalls_ToDo/retrieveToDoList.php?idTopic=' + idTopic +'&labelTitleRank='+ labelTitleRank + '&labelRank=' + labelRank, insertToDoListBefore, 'lastAndInvisible', labelTitleRank, labelRank)		
 		}
 		else {
 			for (var i = 0; i < numberOfToDo ; i++) {
@@ -91,14 +93,16 @@ function displayToDoList (rankOfTitleLabel, rankOfLabel, isChecked) {
 			}
 		}
 	}
-	else {
-		for (var j = 0; j < numberOfToDo ; j++) {
-			aDOMHasClassOfToDo[i].style.display = 'none';
+	else { // unchecked
+		if (numberOfToDo !== 0) { // ça peut arriver ?	
+			for (var j = 0; j < numberOfToDo ; j++) {
+				aDOMHasClassOfToDo[j].style.display = 'none';
+			}
 		}
 	}
 }
 			
-function insertToDoListBefore(sToDoListJSON, idDOMBeforeToInsert) {
+function insertToDoListBefore(sToDoListJSON, idDOMBeforeToInsert, labelTitleRank, labelRank) {
 	var oToDoListJSONParsed = sToDoListJSON == "" ? "" : JSON.parse(sToDoListJSON); 
 	// if oToDoListJSONParsed =="" afficher "pas encore de notes" : non à mettre en dehors de cette function
 	var i = 0;
@@ -108,7 +112,7 @@ function insertToDoListBefore(sToDoListJSON, idDOMBeforeToInsert) {
 		oDOMToDo.id = 'toDo'+(i+IdOfFirstToDo);
 		addContextMenu(oDOMToDo);
 		oDOMToDo.innerHTML = sContent; 
-		oDOMToDo.className = "toDo";
+		oDOMToDo.className = 'toDo toDo'+labelTitleRank+'a'+labelRank;
 		oDOMToDo.draggable = "true";
 		oDOMToDo.idInDdb = x;
 		oDOMToDo.dateCreation = oToDoListJSONParsed[x][1];
@@ -158,7 +162,7 @@ function submitToDo(){
 	if (toDoContent !=="") {
 		IdOfFirstToDo -=1;
 		var oDOMToInsertBefore = document.getElementById('toDo'+ IdOfFirstToDoInitial) === null ? 'lastAndInvisible' : 'toDo' + parseInt(IdOfFirstToDo + 1); 
-		ajaxCall('phpAjaxCalls_ToDo/addToDo.php?idTopic=' + idTopic + "&toDoContent=" + toDoContent + "&labels=11", insertToDoListBefore, oDOMToInsertBefore); // oDOMToInsertBefore est une string ici...
+		ajaxCall('phpAjaxCalls_ToDo/addToDo.php?idTopic=' + idTopic + "&toDoContent=" + toDoContent + "&labels=11", insertToDoListBefore, oDOMToInsertBefore, "1", "1"); // oDOMToInsertBefore est une string ici...
 	}
 }
 
